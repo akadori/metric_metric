b628796058bfc4d1133abe867b1db387bd962455
tamaritamari <shuhei.tamari@cureapp.jp>
2023-10-09 22:44:37 +0900

    remove dist

--- dist/demo/bye.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bye = void 0;
-const byebye_1 = require("./byebye");
-console.log(byebye_1.byebye);
-exports.bye = "bye";
